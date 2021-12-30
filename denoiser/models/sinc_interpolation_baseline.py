9ab7ecd8f4b513b7cacc4bb8fa711dcb4cdf7572
ortal <ortal1602@gmail.com>
2021-12-30 15:21:01 +0200

    added sinc interpolation baseline

--- denoiser/models/sinc_interpolation_baseline.py
+++ denoiser/models/sinc_interpolation_baseline.py
@@ -1,15 +1,100 @@
+import json
+import math
+import os
+
 import torch
 import argparse
 from torchaudio.transforms import Resample
-from denoiser.data import NoisyCleanSet
+
+from denoiser.audio import Audioset
 from denoiser.evaluate import get_pesq, get_stoi
 from concurrent.futures import ThreadPoolExecutor
+
+from denoiser.resample import downsample2
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--path_to_json_dir")
 parser.add_argument("--src_sr", default=8000, required=False)
 parser.add_argument("--target_sr", default=16000, required=False)
 args = parser.parse_args()
+
+def match_files(noisy, clean, matching="sort"):
+    """match_files.
+    Sort files to match noisy and clean filenames.
+    :param noisy: list of the noisy filenames
+    :param clean: list of the clean filenames
+    :param matching: the matching function, at this point only sort is supported
+    """
+    noisy.sort()
+    clean.sort()
+
+class NoisyCleanSet:
+    def __init__(self, json_dir, calc_valid_length_func, matching="sort", clean_length=None, stride=None,
+                 pad=True, sample_rate=None, scale_factor=1, with_path=False, is_training=False, mel_config:MelSpecConfig=None):
+        """__init__.
+        :param json_dir: directory containing both clean.json and noisy.json
+        :param matching: matching function for the files
+        :param clean_length: maximum sequence length
+        :param stride: the stride used for splitting audio sequences
+        :param pad: pad the end of the sequence with zeros
+        :param sample_rate: the signals sampling rate
+        """
+        self.scale_factor = scale_factor
+        self.with_path = with_path
+        self.clean_length = clean_length
+        self.calc_valid_length_func = calc_valid_length_func
+        self.is_training = is_training
+
+        if self.is_training:
+            input_training_length = math.ceil(self.clean_length / self.scale_factor)
+            self.valid_length = self.calc_valid_length_func(input_training_length)
+        else:
+            self.valid_length = None
+
+        noisy_json = os.path.join(json_dir, 'noisy.json')
+        clean_json = os.path.join(json_dir, 'clean.json')
+        with open(noisy_json, 'r') as f:
+            noisy = json.load(f)
+        with open(clean_json, 'r') as f:
+            clean = json.load(f)
+
+        match_files(noisy, clean, matching)
+        kw = {'length': self.valid_length, 'stride': stride, 'pad': pad, 'with_path': with_path}
+        self.clean_set = Audioset(clean, sample_rate=sample_rate, **kw, mel_config=mel_config)
+        self.noisy_set = Audioset(noisy, sample_rate=sample_rate, **kw, mel_config=mel_config)
+
+        assert len(self.clean_set) == len(self.noisy_set)
+
+    def _process_data(self, noisy, clean):
+
+        if self.scale_factor == 2:
+            noisy = downsample2(noisy)
+        elif self.scale_factor == 4:
+            noisy = downsample2(noisy)
+            noisy = downsample2(noisy)
+        elif self.scale_factor != 1:
+            raise RuntimeError(f"Scale factor should be 1, 2, or 4")
+
+        return noisy, clean
+
+    def _get_item_with_path(self, index):
+        (noisy, noisy_path), (clean, clean_path) = self.noisy_set[index], self.clean_set[index]
+        noisy, clean = self._process_data(noisy, clean)
+        return (noisy, noisy_path), (clean, clean_path)
+
+    def _get_item_without_path(self, index):
+        noisy, clean = self.noisy_set[index], self.clean_set[index]
+        noisy, clean = self._process_data(noisy, clean)
+        return noisy, clean
+
+    def __getitem__(self, index):
+        if self.with_path:
+            return self._get_item_with_path(index)
+        else:
+            return self._get_item_without_path(index)
+
+    def __len__(self):
+        return len(self.noisy_set)
 
 
 class SincIntrpolationBaseline:
